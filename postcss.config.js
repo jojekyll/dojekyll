35566a28a425297bd492fa65054441263a632b58
Antonio Trento <antoniotrento83@gmail.com>
2020-11-09 23:33:16 +0100

    edit

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    module.exports = {
+        use: 'rucksack-css',
+        plugins: [
+          require('rucksack-css')({ url: 'copy', useHash: true }),
+          require('autoprefixer')
+        ],
+      }
+};
